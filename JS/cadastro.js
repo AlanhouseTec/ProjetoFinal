663461f59d65e557223cbc0225860c0c730d3223
Talles2305 <tco4@aluno.ifnmg.edu.br>
2024-10-08 19:34:20 -0300

    0.8
    Co-authored-by: Rafa3339

--- JS/cadastro.js
+++ JS/cadastro.js
@@ -3,13 +3,19 @@ const merc = document.getElementById('merc');
 const config = document.getElementById('config');
 const funcionarios = document.getElementById('funcionarios');
 const novoFunc = document.getElementById('novofunc');
-const adicionarfunc = document.getElementById('adicionarfunc');
+const adicionarFunc = document.getElementById('adicionarfunc');
 const addFunc = document.getElementById('addfunc');
 const mercadorias = document.getElementById('mercadorias');
 const novaMerc = document.getElementById('novamerc');
-const adicionarMerc = document.getElementById('adiconarmerc');
+const adicionarMerc = document.getElementById('adicionarmerc');
 const addMerc = document.getElementById('addmerc');
 const configuracoes = document.getElementById('configuracoes');
+const saveConfig = document.getElementById('saveconfig');
+const fecharFunc = document.getElementById('fecharfunc');
+const fecharAddFunc = document.getElementById('fecharaddfunc');
+const fecharMerc = document.getElementById('fecharmerc');
+const fecharAddMerc = document.getElementById('fecharaddmerc');
+var tamanho;
 
 func.addEventListener('click', () => {
     if (mercadorias.style.display === 'flex'){
@@ -19,23 +25,30 @@ func.addEventListener('click', () => {
         configuracoes.style.display = 'none';
     }
     if (novoFunc.style.display === 'flex'){ 
-        novoFunc.style.display = 'none';
+        novoFunc.style.display = 'none';    
     }
-    if (funcionarios.style.display === 'flex') {
-        funcionarios.style.display = 'none';
-    } else {
-        funcionarios.style.display = 'flex';
-    }
+    funcionarios.style.display = 'flex';
 });
-adicionarfunc.addEventListener('click', () => {
-    if (novoFunc.style.display === 'flex') {
-        novoFunc.style.display = 'none';
-    } else {
+adicionarFunc.addEventListener('click', () => {
+    if(novoFunc.style.display === 'none'){
+        tamanho = funcionarios.offsetHeight;
         novoFunc.style.display = 'flex';
+        funcionarios.style.height = novoFunc.offsetHeight + 30 + 'px';
     }
 });
 addFunc.addEventListener('click', () => {
     novoFunc.style.display = 'none';
+    funcionarios.style.height = tamanho + 'px';
+});
+
+fecharFunc.addEventListener('click', () =>{
+    if(!(novoFunc.style.display  === 'flex')){
+        funcionarios.style.display = 'none';
+    }
+})
+fecharAddFunc.addEventListener('click', () =>{
+    novoFunc.style.display = 'none';
+    funcionarios.style.height = tamanho + 'px';
 });
 
 merc.addEventListener('click', () => {
@@ -45,7 +58,7 @@ merc.addEventListener('click', () => {
     if (configuracoes.style.display === 'flex'){
         configuracoes.style.display = 'none';
     }
-    if (novaMerc.style.display === 'grid'){
+    if (novaMerc.style.display === 'flex'){
         novaMerc.style.display = 'none';
     }
     if (mercadorias.style.display === 'flex') {
@@ -54,16 +67,29 @@ merc.addEventListener('click', () => {
         mercadorias.style.display = 'flex';
     }
 });
-adicionarmerc.addEventListener('click', () => {
-    if (novaMerc.style.display === 'grid') {
-        novaMerc.style.display = 'none';
-    } else {
-        novaMerc.style.display = 'grid';
+adicionarMerc.addEventListener('click', () => {
+    if(novaMerc.style.display === 'none'){
+        tamanho = mercadorias.offsetHeight;
+        novaMerc.style.display = 'flex';
+        mercadorias.style.height = novaMerc.offsetHeight + 30 + 'px';
     }
 });
 addMerc.addEventListener('click', () => {
     novaMerc.style.display = 'none';
+    mercadorias.style.height = tamanho + 'px';
 });
+
+fecharMerc.addEventListener('click', () =>{
+    if(!(novaMerc.style.display  === 'flex')){
+        mercadorias.style.display = 'none';
+    }
+})
+
+fecharAddMerc.addEventListener('click', () =>{
+    novaMerc.style.display = 'none';
+    mercadorias.style.height = tamanho + 'px';
+});
+
 
 config.addEventListener('click', () => {
     if (funcionarios.style.display === 'flex') {
@@ -77,4 +103,7 @@ config.addEventListener('click', () => {
     } else {
         configuracoes.style.display = 'flex';
     }
+});
+saveConfig.addEventListener('click', () =>{
+    configuracoes.style.display = 'none';
 });
